a08d72dfe549f409d053b584b3a1576b1c5816fe
signal0801 <daybreak.a.signal@gmail.com>
2017-01-24 03:22:10 +0900

    change layout

--- board.php
+++ board.php
@@ -46,30 +46,38 @@ if ($dblink !== false) {
     arsort($data);
 
 } else {
-    echo "データベースの接続に失敗しました";
+    die('接続失敗です。'.mysql_error());
 }
 
 //disconnect database
 mysqli_close( $dblink );
 ?>
-
-<html>
+<!DOCTYPE html>
+<html lang="ja">
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     </head>
     <body>
+<h1>簡易掲示板</h1>
+<section>
+<h2>新規投稿</h2>
         <form method="post" action="">
-            名前<input type="text" name="name" value="" />
-            コメント<textarea name="comment" rows="4" cols="20"></textarea>
+            名前（10字以内）<input type="text" name="name" value="" maxlength = "10" /><br>
+            コメント<textarea name="comment" rows="4" cols="20" maxlength = "100" wrap="hard" placeholder="100字以内で入力してください"></textarea>
            <input type="submit" name="send" value="書き込む" />
         </form>
         <!-- ここに、書き込まれたデータを表示する -->
+</section>
+<section>
+<h2>投稿一覧</h2>
 <?php
 if ( $msg     !== '' ) echo '<p>' . $msg . '</p>';
 if ( $err_msg !== '' ) echo '<p style="color:#f00;">' . $err_msg . '</p>';
 foreach( $data as $key => $val ){
-    echo $val['name']."さんのコメント" . ' ' . $val['comment'] . '<br>';
+    echo $val['name']."さんのコメント" . '<br>';
+    echo $val['comment'].'<br>';
 }
 ?>
+</section>
     </body>
 </html>
